3ce8f758ddae368ce647bbbeb6ee693e31f95c6e
Nitesh yadav <105155378+nitesh7003@users.noreply.github.com>
2024-09-20 11:33:11 +0530

    Update Product.cs

--- SampleMVCAppDB/Models/Product.cs
+++ SampleMVCAppDB/Models/Product.cs
@@ -1,11 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleMVCAppDB.Models
 {
     public class Product
     {
         [Key]
-        public int ProductID { get; set; }  // Primary key, no validation needed
+        public int ProductID { get; set; }  
 
         [Required(ErrorMessage = "Product Name is required")]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Product Name must be between 2 and 100 characters")]
